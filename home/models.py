62c65b465240f0e2d6a43851a2d075406de93a33
Suji-Tech <sujith.jeewantha@gmail.com>
2022-03-23 22:59:58 +0530

    improved landing page responsivness

--- home/models.py
+++ home/models.py
@@ -3,20 +3,25 @@ from re import template
 from django.db import models
 from django.shortcuts import render
 
+from wagtail.search import index
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 
 
-class HomePage(RoutablePageMixin, Page):
+class HomePage(Page):
+    pass
     
-    templates = "home/home_page.html"
-    # templates = "home/sub_page.html"
-    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    # templates = "home/home_page.html"
+    # banner_title = models.CharField(max_length=100, blank=False, null=True)
 
-    contant_panels = Page.content_panels + [
-        FieldPanel('banner_title')
-    ]
+    # search_fields = Page.search_fields + [
+    #     # index.SearchField('banner_title'),
+    # ]
+
+    # content_panels = Page.content_panels + [
+    #     FieldPanel('banner_title'),
+    # ]
 
     # @route(r'^subpage/$')
     # def  the_sub_page(self, request, *args, **kwargs):
--- subpage/models.py
+++ subpage/models.py
@@ -1,16 +1,46 @@
+from pydoc import classname
 from django.db import models
 
+from wagtail.search import index
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
 # Create your models here.
 
 class SubPage(Page):
+    pass
 
-    template = "subpage/sub_page.html"
+    # template = "subpage/sub_page.html"
 
-    subtitle = models.CharField(max_length=100, null=True, blank=True)
+    # subtitle = models.CharField(max_length=100, null=True, blank=True)
+    # short_description = models.CharField(max_length=250)
+    # body = RichTextField(blank=True)
 
-    contant_panels = Page.content_panels + [
-        FieldPanel('subtitle')
+class ContentPage(Page):
+    # date = models.DateField("Post date")
+    short_description = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('short_description'),
+        index.SearchField('body'),
     ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('short_description'),
+        FieldPanel('body', classname="full"),
+    ]
+
+    # search_fields = Page.search_fields + [
+    #     index.SearchField('subtitle'),
+    #     index.SearchField('short_description'),
+    #     index.SearchField('body'),
+    # ]
+
+    # content_panels = Page.content_panels + [
+    #     FieldPanel('subtitle'),
+    #     FieldPanel('short_description'),
+    #     FieldPanel('body', classname="full"),
+    # ]
+
