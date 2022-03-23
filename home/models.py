0836b8e2b348d896c07c9f87560dbc684c3e8c3c
Suji-Tech <sujith.jeewantha@gmail.com>
2022-03-23 09:00:05 +0530

    basic theme added

--- home/models.py
+++ home/models.py
@@ -1,7 +1,15 @@
+from re import template
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    
+    templates = "home/home_page.html"
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+
+    contant_panels = Page.content_panels + [
+        FieldPanel('banner_title')
+    ]
