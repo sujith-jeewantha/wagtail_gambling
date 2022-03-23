25458d1290176378942eb861abd0279ee0c0eec8
Suji-Tech <sujith.jeewantha@gmail.com>
2022-03-23 08:16:18 +0530

    first commit

---
+++ gambling_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-rhn%$sv#roa%lqj91dej!m@h^y*+y)p=itf#$2b+#6pf$w9ef*'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
